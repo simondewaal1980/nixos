7fb470ba3090c2e7507763e2698aa1606dfd0817
simondewaal1980 <119229538+simondewaal1980@users.noreply.github.com>
2023-12-28 15:07:59 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -8,7 +8,7 @@
  imports =
    [ # Include the results of the hardware scan.
      ./hardware-configuration.nix
-   <home-manager/nixos>
+   #<home-manager/nixos>
    ];
      nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
